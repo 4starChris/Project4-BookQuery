4ccc2736ceeebb37e027ffb36fd96278f8ae641c
4starChris <4star.christian@gmail.com>
2023-05-08 16:53:34 +1200

    Linq query works now. A new method was used to sort. Am keeping the old method commented out as I want to figure out how to make that work too.

--- Project4 BookQuery/Program.cs
+++ Project4 BookQuery/Program.cs
@@ -17,20 +17,114 @@ class Program
 {
     public static void Main(string[] args)
     {
+        
         string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, @"BookJson.csv");
         string bookjson = File.ReadAllText(path);
-        Console.WriteLine(bookjson);
+        bool endapp = false;
+
         
-        List<Book> bookList = JsonConvert.DeserializeObject<List<Book>>(bookjson);
+        Func<Book, bool> searchby;
+        string searchterm;
+        Func<Book, string> sortby; //Old sorting method. It is meant to output a TKey, not a string. But the code doesn't recognise TKey, and I can't figure out what that is.------------------------------------
+
+        while (!endapp)
+        {
+            //Console.WriteLine(bookjson);
+
+            List<Book> bookList = JsonConvert.DeserializeObject<List<Book>>(bookjson);
+
+            Console.WriteLine("Enter search term:");
+            searchterm = Console.ReadLine();
+
+            Console.WriteLine("Would you like to search by A:title, B: author, C: ISBN?");
+            string response = Console.ReadLine().ToLower();
+
+            //-------------------------------------------------------------------------
+            //Filter out books that don't contain the search term
+            //-------------------------------------------------------------------------
+            if (response == "a")
+            {
+                searchby = s => s.title.Contains(searchterm);
+            }
+            else if (response == "b")
+            {
+                searchby = s => s.author.Contains(searchterm);
+            }
+            else if (response == "c")
+            {
+                searchby = s => s.isbn.Contains(searchterm);
+            }
+            else
+            {
+                Console.WriteLine("Please select a valid option!");
+                continue;
+            }
 
 
-        //Search here
-        var searchResults = from book in bookList where book.subtitle.Contains("and") select book;
-        
-        //Print title of books in search
-        foreach (Book book in searchResults){
-            Console.WriteLine(book.title);
+
+            Console.WriteLine("Sort by A: title, B: author, C: ISBN?");
+            response = Console.ReadLine().ToLower();
+
+            /* Old sorting attempt
+
+            //-------------------------------------------------------------------------
+            //I am trying to sort the LINQ query at the end based on the user response. 
+            //-------------------------------------------------------------------------
+            if (response == "a")
+            {
+                sortby = s => s.title;
+            }
+            else if (response == "b")
+            {
+                sortby = s => s.author;
+            }
+            else if (response == "c")
+            {
+                sortby = s => s.isbn;
+            }
+            else
+            {
+                Console.WriteLine("Please select a valid option!");
+                continue;
+            }
+
+            */
+
+            //Search here
+            var searchResults = from book in bookList
+                                //orderby sortby descending //Old sorting attempt
+                                where searchby(book) 
+                                select book;
+
+            //-------------------------------------------------------------------------
+            //Sort search result
+            //-------------------------------------------------------------------------
+            if (response == "a")
+            {
+                searchResults = searchResults.OrderBy(c => c.title);
+            }
+            else if (response == "b")
+            {
+                searchResults = searchResults.OrderBy(c => c.author);
+            }
+            else if (response == "c")
+            {
+                searchResults = searchResults.OrderBy(c => c.isbn);
+            }
+            else
+            {
+                Console.WriteLine("Please select a valid option!");
+                continue;
+            }
+
+            //Print title of books in search
+            foreach (Book book in searchResults)
+            {
+                Console.WriteLine(book.title);
+            } 
+            endapp = true;
         }
+        
         
 
     }
