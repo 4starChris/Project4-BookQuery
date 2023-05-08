c1c20686d2588fd2188a0b978a1b496fe6d7ce1e
4starChris <4star.christian@gmail.com>
2023-05-08 14:11:48 +1200

    Gets local csv file called BookJson. Will deserialise into a list of Book objects. Can do a LINQ search and posts the title of each book.

---
+++ Project4 BookQuery/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4_BookQuery
+{
+    public class Book
+    {
+        public string isbn;
+        public string title;
+        public string subtitle;
+        public string author;
+        public DateTime date;
+        public string publisher;
+        public int pages;
+        public string description;
+        public string website;
+
+        public Book(string isbn, string title, string subtitle, string author, DateTime date, string publisher, int pages, string description, string website)
+        {
+            this.isbn = isbn;
+            this.title = title;
+            this.subtitle = subtitle;
+            this.author = author;
+            this.date = date;
+            this.publisher = publisher;
+            this.pages = pages;
+            this.description = description;
+            this.website = website;
+        }
+
+    }
+}
--- Project4 BookQuery/Program.cs
+++ Project4 BookQuery/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿/* Task
+ * Write a console application that reads in a CSV file containing information about books and allows the user to search for books based on various criteria. Here are the basic requirements for this task:
+ * 
+ * Read in the CSV file using the File.ReadAllLines() method to create an array of strings, where each string represents a line in the file.
+ * Use LINQ to parse the CSV data into a list of book objects. Each book object should have the following properties: title, author, publication year, and ISBN.
+ * Allow the user to search for books by title, author, publication year, or ISBN. Display the search results to the user in the console.
+ * Add the ability for the user to sort the search results by any of the book properties.
+ * 
+ * I am using the data provided by nanotaboada for this exercise: https://gist.github.com/nanotaboada/6396437
+ */
+using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Project4_BookQuery;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, @"BookJson.csv");
+        string bookjson = File.ReadAllText(path);
+        Console.WriteLine(bookjson);
+        
+        List<Book> bookList = JsonConvert.DeserializeObject<List<Book>>(bookjson);
+
+
+        //Search here
+        var searchResults = from book in bookList where book.subtitle.Contains("and") select book;
+        
+        //Print title of books in search
+        foreach (Book book in searchResults){
+            Console.WriteLine(book.title);
+        }
+        
+
+    }
+}
